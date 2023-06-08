39b241d41f426781dcca25cc64ffcd21cfcd2d48
asungy <62207329+asungy@users.noreply.github.com>
2023-06-07 22:24:24 -0400

    Refactored user directory

--- README.md
+++ README.md
@@ -3,11 +3,14 @@
 My SWE loadout.
 
 ## To-Do
+- Set up Home Manager
+  - https://nixos.wiki/wiki/Home_Manager
+- Configure Firefox browser
 - Configure Git
   - https://nixos.wiki/wiki/Git
-- Configure Firefox browser
 - Add NerdFont
 - Figure out what Flakes are
   - https://nixos.wiki/wiki/Flakes
   - https://www.tweag.io/blog/2020-05-25-flakes/
 - Try configuring Wayland for windowing
+  - https://nixos.wiki/wiki/Sway
---
+++ nixos/user/alek.nix
@@ -0,0 +1,40 @@
+# User configuration for "alek".
+
+{ config, pkgs, ... }:
+let
+  username = "alek";
+in
+{
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.alek = {
+    isNormalUser = true;
+    description = username;
+    extraGroups = ["networkmanager" "wheel"];
+    packages = with pkgs; [
+      brave
+      delta
+      dropbox
+      firefox
+      git
+      keepassxc
+      ripgrep
+      tmux
+      xclip
+    ];
+  };
+
+  # Don't prompt for sudo password.
+  #
+  # Reference: https://discourse.nixos.org/t/dont-prompt-a-user-for-the-sudo-password/9163/2
+  security.sudo.extraRules = [
+    {
+      users = [username];
+      commands = [
+        {
+          command = "ALL";
+          options = ["NOPASSWD"];
+        }
+      ];
+    }
+  ];
+}
