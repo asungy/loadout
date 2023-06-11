adedb1fcc35db8d6ff808a68aee63176921f4a62
asungy <62207329+asungy@users.noreply.github.com>
2023-06-11 18:19:19 -0400

    Added Docker

--- system/configuration.nix
+++ system/configuration.nix
@@ -132,6 +132,23 @@
     };
   };
 
+  # Enable Docker.
+  virtualisation = {
+    docker = {
+      enable = true;
+
+      autoPrune = {
+        enable = true;
+        dates = "weekly";
+      };
+
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
