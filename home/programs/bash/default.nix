44378f21c812c9c7ff594bc1dbf0123b7e101ec4
asungy <62207329+asungy@users.noreply.github.com>
2023-07-30 09:00:07 -0400

    9 add more shortcuts (#10)
    * Added maximize pane shortcut for tmux
    
    * Organized long running line in alias config
    
    * Created cpwd bash alias

--- home/programs/bash/default.nix
+++ home/programs/bash/default.nix
@@ -3,7 +3,14 @@
 let
   aliases = {
     archbox = "docker run --network=host -it --rm --name=archbox archlinux:latest /bin/bash";
-    archbox-mnt = "docker run --network=host -it --rm --name=archbox-mnt --workdir=/mnt/\"\$\{PWD##*/}\" --volume=\"\$(pwd)\":/mnt/\"\$\{PWD##*/}\" archlinux:latest /bin/bash";
+    archbox-mnt = (
+      "docker run --network=host -it --rm " +
+      "--name=archbox-mnt " +
+      "--workdir=/mnt/\"\$\{PWD##*/}\" " +
+      "--volume=\"\$(pwd)\":/mnt/\"\$\{PWD##*/}\" " +
+      "archlinux:latest /bin/bash"
+    );
+    cpwd = "pwd | xclip -selection clipboard";
     ls = "ls --color=tty";
     vim = "nvim";
   };
