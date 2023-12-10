c87c35229874c64d88337b47a3dda65822789067
asungy <62207329+asungy@users.noreply.github.com>
2023-12-10 00:20:02 -0500

    Changed nixim to use shorter alias

--- home/programs/bash/default.nix
+++ home/programs/bash/default.nix
@@ -14,7 +14,7 @@ let
     ls = "ls --color=tty";
     li = "ls --color=tty"; # easier typing for dvorak
     vim = "nvim";
-    nixim = "nix run github:asungy/nixim";
+    nxm = "nix run github:asungy/nixim";
   };
   cmdsForInteractiveShell = builtins.readFile ./rc.sh;
 in
