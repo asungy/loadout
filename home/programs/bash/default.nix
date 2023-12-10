1d834debe969c5b50805af4038f3445a59047197
asungy <62207329+asungy@users.noreply.github.com>
2023-12-09 22:31:25 -0500

    Made alias for nixim

--- home/programs/bash/default.nix
+++ home/programs/bash/default.nix
@@ -14,6 +14,7 @@ let
     ls = "ls --color=tty";
     li = "ls --color=tty"; # easier typing for dvorak
     vim = "nvim";
+    nixim = "nix run github:asungy/nixim";
   };
   cmdsForInteractiveShell = builtins.readFile ./rc.sh;
 in
