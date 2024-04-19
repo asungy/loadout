7e3eb319c4d9b54e3445e8780b9c71494b1ddf4e
asungy <62207329+asungy@users.noreply.github.com>
2024-04-18 23:51:43 -0400

    Updated nix flake

--- system/configuration.nix
+++ system/configuration.nix
@@ -103,7 +103,7 @@ in {
   services.pcscd.enable = true;
   programs.gnupg.agent = {
     enable = true;
-    pinentryFlavor = "curses";
+    pinentryPackage = pkgs.pinentry-curses;
     enableSSHSupport = true;
   };
 
