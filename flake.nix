86017cd9264232e586bbb0125814fb54dabc17c8
asungy <62207329+asungy@users.noreply.github.com>
2024-04-19 23:15:40 -0400

    Pinning nixpkgs version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,10 @@
   description = "asungy's Home Manager and NixOS configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # Original: nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    #
+    # Pinning nixpkgs until Nvidia stops shitting the bed.
+    nixpkgs.url = "github:NixOS/nixpkgs/bb2b73df7bcfbd2dd55ff39b944d70547d53c267";
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
