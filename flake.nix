da58dfaa84296071d1fbf9d98c4fb9499c34cb25
asungy <62207329+asungy@users.noreply.github.com>
2024-04-19 23:26:02 -0400

    Revert "Pinning nixpkgs version"
    This reverts commit 86017cd9264232e586bbb0125814fb54dabc17c8.

--- flake.nix
+++ flake.nix
@@ -2,10 +2,7 @@
   description = "asungy's Home Manager and NixOS configurations";
 
   inputs = {
-    # Original: nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    #
-    # Pinning nixpkgs until Nvidia stops shitting the bed.
-    nixpkgs.url = "github:NixOS/nixpkgs/bb2b73df7bcfbd2dd55ff39b944d70547d53c267";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
