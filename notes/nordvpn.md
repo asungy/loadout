ef4a52698fa0f9a516afc9d6876ef3babb53aa95
asungy <62207329+asungy@users.noreply.github.com>
2024-05-29 20:13:21 -0400

    No need to copy file contents to wgnord config file

--- notes/nordvpn.md
+++ notes/nordvpn.md
@@ -1,21 +1,5 @@
 # Setting up NordVPN
 
-NordVPN isn't fully supported so some manual work is required.
-
 1. Grab an access token from the NordVPN dashboard.
 2. Login using `sudo wgnord login <access token>`
-3. Copy the following text to `/var/lib/wgnord/template.conf`:
-```
-[Interface]
-PrivateKey = PRIVKEY
-Address = 10.5.0.2/32
-MTU = 1350
-DNS = 103.86.96.100 103.86.99.100
-
-[Peer]
-PublicKey = SERVER_PUBKEY
-AllowedIPs = 0.0.0.0/0, ::/0
-Endpoint = SERVER_IP:51820
-PersistentKeepalive = 25
-```
-4. Test connection: `sudo wgnord connect france`
+3. Test connection: `sudo wgnord connect france`
--- system/sway.nix
+++ system/sway.nix
@@ -1,8 +1,9 @@
 { pkgs, inputs, config, ... } :
-  let
-    udevRules = pkgs.callPackage ./udev/default.nix { inherit pkgs; };
-    username = "asungy";
-in {
+let
+  udevRules = pkgs.callPackage ./udev/default.nix { inherit pkgs; };
+  username = "asungy";
+in
+{
   imports = [];
 
   # System packages.
@@ -193,11 +194,41 @@ in {
     };
   };
 
-  # Create directory for wgnord (NordVPN)
-  systemd.tmpfiles.rules = [
-    "d /var/lib/wgnord 0700 root root"
-    "d /etc/wireguard 0700 root root"
-  ];
+  # HACK: Create file/directories for wgnord (NordVPN)
+  system.activationScripts.wgnordTemplateConfig =
+  let
+    wgnordTemplateConfig = pkgs.writeTextFile {
+      name = "template.conf";
+      text = ''
+        [Interface]
+        PrivateKey = PRIVKEY
+        Address = 10.5.0.2/32
+        MTU = 1350
+        DNS = 103.86.96.100 103.86.99.100
+
+        [Peer]
+        PublicKey = SERVER_PUBKEY
+        AllowedIPs = 0.0.0.0/0, ::/0
+        Endpoint = SERVER_IP:51820
+        PersistentKeepalive = 25
+      '';
+    };
+    wgnordConfigDir = "/var/lib/wgnord";
+    wireguardEtcDir = "/etc/wireguard";
+  in
+  ''
+    mkdir -p ${wireguardEtcDir}
+    chown root:root ${wireguardEtcDir}
+    chmod 0700 ${wireguardEtcDir}
+
+    mkdir -p ${wgnordConfigDir}
+    chown root:root ${wgnordConfigDir}
+    chmod 0700 ${wgnordConfigDir}
+
+    cp ${wgnordTemplateConfig} ${wgnordConfigDir}/template.conf
+    chown root:root ${wgnordConfigDir}/template.conf
+    chmod 0700 ${wgnordConfigDir}/template.conf
+  '';
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
