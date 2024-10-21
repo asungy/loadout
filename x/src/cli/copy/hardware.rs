863c055e560768ade55cc454c6438596d493e92d
asungy <62207329+asungy@users.noreply.github.com>
2024-10-20 23:45:26 -0400

    Added cli flag for copy to copy default nixos configs

--- x/src/cli/copy/hardware.rs
+++ x/src/cli/copy/hardware.rs
@@ -9,12 +9,20 @@ pub fn command() -> Box<dyn crate::cli::ExecCommand> {
         }
 
         fn command(&self) -> clap::Command {
-            clap::Command::new(NAME).about("Copy hardware configuration files")
+            clap::Command::new(NAME)
+                .about("Copy hardware configuration files")
+                .arg(clap::Arg::new("default-config")
+                    .short('d')
+                    .long("default-config")
+                    .help("Additionally copy the generated default NixOS configuration")
+                    .action(clap::ArgAction::SetTrue)
+                )
         }
 
         fn callback(&self) -> crate::cli::CommandFn {
-            Box::new(|_: &clap::ArgMatches| -> anyhow::Result<()> {
-                crate::core::hardware::generate_hardware_file()
+            Box::new(|matches: &clap::ArgMatches| -> anyhow::Result<()> {
+                let copy_default_config: &bool = matches.get_one("default-config").expect("At least a default value to be set");
+                crate::core::hardware::generate_hardware_file(*copy_default_config)
             })
         }
     }
--- x/src/core/hardware.rs
+++ x/src/core/hardware.rs
@@ -1,4 +1,4 @@
-pub fn generate_hardware_file() -> anyhow::Result<()> {
+pub fn generate_hardware_file(copy_default_config: bool) -> anyhow::Result<()> {
     let tempdir = tempfile::Builder::new()
         .prefix("nixos-generate-hardware")
         .tempdir()?;
@@ -42,6 +42,27 @@ pub fn generate_hardware_file() -> anyhow::Result<()> {
             Err(anyhow::anyhow!("Error copying hardware configuration file."))
         }
     }(&source)?;
+
+    if copy_default_config {
+        let source = tempdir.path().join("configuration.nix");
+        |source: &std::path::Path| -> anyhow::Result<()> {
+            let dest = std::env::current_dir()?.join("system/machine/framework/default.nix");
+            let output = std::process::Command::new("cp")
+                .args([
+                    source.to_str().unwrap(),
+                    dest.to_str().unwrap(),
+                ])
+                .output()?;
+
+            if output.status.success() {
+                println!("{}", std::str::from_utf8(&output.stdout).unwrap());
+                Ok(())
+            } else {
+                eprintln!("{}", std::str::from_utf8(&output.stderr).unwrap());
+                Err(anyhow::anyhow!("Error copying NixOS configuration file."))
+            }
+        }(&source)?;
+    }
 
     Ok(())
 }
