8698d261e83c542578898e5cf3dd1c8594dd3893
asungy <62207329+asungy@users.noreply.github.com>
2024-12-08 11:27:57 -0500

    simple counter app

--- x/src/main.rs
+++ x/src/main.rs
@@ -32,13 +32,34 @@ impl App {
 
     fn handle_events(&mut self) -> std::io::Result<()> {
         match event::read()? {
-            Event::Key(key_event) => if key_event.kind == KeyEventKind::Press => {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
                 self.handle_key_event(key_event);
             },
             _ => {},
         }
 
         Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left => self.decrement_counter(),
+            KeyCode::Right => self.increment_counter(),
+            _ => {},
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn increment_counter(&mut self) {
+        self.counter += 1;
+    }
+
+    fn decrement_counter(&mut self) {
+        self.counter -= 1;
     }
 }
 
