d67afd307a24aa68e43d6a3887239b97518e1583
asungy <62207329+asungy@users.noreply.github.com>
2024-12-27 16:29:41 -0500

    Added ssh configurations

--- system/spytower.nix
+++ system/spytower.nix
@@ -28,6 +28,19 @@ in
     ++ packages.misc
     ;
 
+  # Reference: https://nixos.wiki/wiki/SSH
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = null;
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "no";
+    };
+  };
+
   # NOTE - this takes a long time to build.
   # virtualisation.virtualbox = {
   #   host.enable = true;
