0c0100d55392fa76ecde8ed5002a30b55ef78180
asungy <62207329+asungy@users.noreply.github.com>
2025-10-16 14:36:13 -0400

    Added spytower ssh public key to labboi

--- system/labboi/user.nix
+++ system/labboi/user.nix
@@ -8,6 +8,7 @@
     password = "";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOSAEaZXnJmHWh5s6FT+XIPCS2qC+XDsjH6ECDrKfxD1 u0_a317@localhost"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL0tQTPfgUokkNR0aKsBVFs+tf+efUyx7T7cK4WuxhJh asungy@spytower"
     ];
   };
   users.groups.plugdev.members = [ username ];
