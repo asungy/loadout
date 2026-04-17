5e25dcfefc1dcc3c99a5b02cdd023e0f103fa217
asungy <62207329+asungy@users.noreply.github.com>
2026-04-17 00:58:49 -0400

    Updated labboi config

--- system/labboi.nix
+++ system/labboi.nix
@@ -1,14 +1,31 @@
 { pkgs, ... } :
+let
+  packages = import ./packages.nix { inherit pkgs; };
+in
 {
   imports = [
+    ./modules/audio.nix
+    ./modules/environment_vars.nix
+    ./modules/fonts.nix
+    ./modules/gpg.nix
+    ./modules/graphics
+    ./modules/i18n.nix
+    ./modules/networking.nix
     ./modules/nix_daemon.nix
+    ./modules/tablet.nix
+    ./modules/udev
     ./modules/ssh.nix
     ./modules/tailscale.nix
     ./modules/user.nix
+    ./modules/virtualisation.nix
+    ./modules/wayland.nix
   ];
 
-  networking.networkmanager.enable = true;
-  networking.firewall.enable = true;
-
+  environment.systemPackages = []
+    ++ packages.desktop
+    ++ packages.apps
+    ++ packages.misc
+    ;
+    
   system.stateVersion = "25.05"; # Did you read the comment?
 }
